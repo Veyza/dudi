9715adf22f9545f319877675608237b79b3f22b5
veyza <vveyzaa@gmail.com>
2025-10-16 14:24:15 +0300

    tabs are replaced by 4 spaces

--- examples/main_program.f90
+++ examples/main_program.f90
@@ -15,44 +15,44 @@
 ! E-mail: vveyzaa@gmail.com
 
 program main_program
-	use const
-	use define_types
-	use integrator
-	use inputdata
-	use dataoutmod
-	USE OMP_LIB
+    use const
+    use define_types
+    use integrator
+    use inputdata
+    use dataoutmod
+    USE OMP_LIB
 
-	implicit none
-	! number of sources
-	integer, parameter :: Ns = 1
-	! number of points on the SC trajectory
-	! for which the number density is to be calculated
-	integer, parameter :: nt = 1
-	real(8), parameter :: tnow = 600d0
-	integer i_s, i, ii
-	real density(nt,2), tmp_res(nt,2)
-	type(source_properties) source(Ns)
-	type(position_in_space) point(nt)
+    implicit none
+    ! number of sources
+    integer, parameter :: Ns = 1
+    ! number of points on the SC trajectory
+    ! for which the number density is to be calculated
+    integer, parameter :: nt = 1
+    real(8), parameter :: tnow = 600d0
+    integer i_s, i
+    real density(nt,2), tmp_res(nt,2)
+    type(source_properties) source(Ns)
+    type(position_in_space) point(nt)
 
-	call read_sources_params(source, Ns, './input_data_files/sources.dat')
-	call read_spacecraft_coordinates(point, nt, './input_data_files/points.dat')
+    call read_sources_params(source, Ns, './input_data_files/sources.dat')
+    call read_spacecraft_coordinates(point, nt, './input_data_files/points.dat')
 
-	density = 0d0
+    density = 0d0
 
-	do i_s = 1, Ns
+    do i_s = 1, Ns
 
-		!$OMP PARALLEL PRIVATE(i) &
-		!$OMP SHARED(i_s, point, source, tmp_res)
-		!$OMP DO
-		do i = 1, nt
-			call DUDI(tmp_res(i,:), point(i), source(i_s), tnow)
-		enddo
-		!$OMP END DO
-		!$OMP END PARALLEL
-		density = density + tmp_res
-	enddo
+        !$OMP PARALLEL PRIVATE(i) &
+        !$OMP SHARED(i_s, point, source, tmp_res)
+        !$OMP DO
+        do i = 1, nt
+            call DUDI(tmp_res(i,:), point(i), source(i_s), tnow)
+        enddo
+        !$OMP END DO
+        !$OMP END PARALLEL
+        density = density + tmp_res
+    enddo
 
-	call result_out(density, nt, point)
+    call result_out(density, nt, point)
 
 
 
